0c452b21cc1a59092f72a0bd548349a7c8510510
joaovitor-ribeiro <joaosvit10@hotmail.com>
2021-08-26 17:01:15 -0300

    Projeto criado

---
+++ src/main/java/br/com/projetoxp/ProjetoxpApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoxpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoxpApplication.class, args);
+	}
+
+}

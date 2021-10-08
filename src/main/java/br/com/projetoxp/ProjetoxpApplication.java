04cc9af78d9f5683cb02890a216528a86dd254fe
vitor-ops <vitor_hugonascimento@hotmail.com>
2021-10-08 19:21:40 -0300

    Encriptacao

--- src/main/java/br/com/projetoxp/ProjetoxpApplication.java
+++ src/main/java/br/com/projetoxp/ProjetoxpApplication.java
@@ -2,12 +2,21 @@ package br.com.projetoxp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProjetoxpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoxpApplication.class, args);
 	}
 
+	@Bean
+    public PasswordEncoder getPassWordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 }
